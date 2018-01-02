7de2167ae07d2eb422413119d3db48146f86a4c4
Sablin Igor <sablin.igor@gmail.com>
2018-01-03 00:25:15 +0300

    примера Factory Method

---
+++ src/main/java/edu/pattern/creational/factorymethod/Dog.java
@@ -0,0 +1,29 @@
+package edu.pattern.creational.factorymethod;
+
+public interface Dog {
+    void speak ();
+}
+
+class Poodle implements Dog
+{
+    public void speak()
+    {
+        System.out.println("The poodle says \"arf\"");
+    }
+}
+
+class Rottweiler implements Dog
+{
+    public void speak()
+    {
+        System.out.println("The Rottweiler says (in a very deep voice) \"WOOF!\"");
+    }
+}
+
+class SiberianHusky implements Dog
+{
+    public void speak()
+    {
+        System.out.println("The husky says \"Dude, what's up?\"");
+    }
+}
